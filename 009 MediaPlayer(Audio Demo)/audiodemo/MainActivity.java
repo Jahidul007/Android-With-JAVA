c99544054781d0f03530e4ab256a76465798eefe
Jahidul Islam <jahidul479@gmail.com>
2018-05-30 00:02:07 +0600

    audio demo

---
+++ 009 MediaPlayer(Audio Demo)/audiodemo/MainActivity.java
@@ -0,0 +1,34 @@
+package example.com.audiodemo;
+
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.bird);
+
+        Button button = (Button) this.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+
+
+    }
+}
+
