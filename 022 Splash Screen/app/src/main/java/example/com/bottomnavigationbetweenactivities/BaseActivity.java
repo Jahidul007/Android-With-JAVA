c33282a4bea5360e1d7a5c9dfc9cc67c4e6bbc3e
Jahidul Islam <jahidul479@gmail.com>
2018-06-24 13:29:29 +0600

    splash screen

---
+++ 022 Splash Screen/app/src/main/java/example/com/bottomnavigationbetweenactivities/BaseActivity.java
@@ -0,0 +1,75 @@
+package example.com.bottomnavigationbetweenactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public abstract class BaseActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener{
+
+    protected BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        BottomNavigationViewHelper.disableShiftMode(navigationView);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateNavigationBarState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.navigation_home) {
+            startActivity(new Intent(this, HomeActivity.class));
+        } else if (itemId == R.id.navigation_dashboard) {
+            startActivity(new Intent(this, DashboardActivity.class));
+        } else if (itemId == R.id.news) {
+            startActivity(new Intent(this, MainActivity_news.class));
+        }
+        overridePendingTransition(0, 0);
+       // finish();
+        return true;
+    }
+
+    private void updateNavigationBarState() {
+        int actionId = getNavigationMenuItemId();
+        selectBottomNavigationBarItem(actionId);
+    }
+
+    void selectBottomNavigationBarItem(int itemId) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            boolean shouldBeChecked = item.getItemId() == itemId;
+            if (shouldBeChecked) {
+                item.setChecked(true);
+                break;
+            }
+        }
+    }
+
+    abstract int getContentViewId();
+
+    abstract int getNavigationMenuItemId();
+}
