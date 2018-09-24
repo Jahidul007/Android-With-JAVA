9b3e9b6626239f9e8d7dd6b4046dfb04adea587d
Jahidul Islam <jahidul479@gmail.com>
2018-09-24 23:48:07 +0600

    init commit

---
+++ 027 Shared Preferences/app/src/main/java/example/com/sharedpreferences/MainActivity.java
@@ -0,0 +1,51 @@
+package example.com.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // System.out.println("jahid");
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("example.com.sharedpreferences", Context.MODE_PRIVATE);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Don");
+        arrayList.add("Donkey");
+        arrayList.add("Bodka");
+        arrayList.add("Pok pok");
+
+        try {
+            sharedPreferences.edit().putString("arrayList",ObjectSerializer.serialize(arrayList)).apply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> newArrayList = new ArrayList<>();
+
+        try {
+            newArrayList  = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("arrayList",
+                    ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("ArrayList ", newArrayList.toString());
+        // sharedPreferences.edit().putString("username","Jahid").apply();
+        // String username  = sharedPreferences.getString("username","");
+
+       // System.out.println("Username: " + username);
+    }
+}
