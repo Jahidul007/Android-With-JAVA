75be6fee394eb9afc27a0c3f005cc6c21058d824
Jahidul Islam <jahidul479@gmail.com>
2018-09-25 00:01:51 +0600

    customising action bar

--- 027 Shared Preferences/app/src/main/java/example/com/sharedpreferences/MainActivity.java
+++ 027 Shared Preferences/app/src/main/java/example/com/sharedpreferences/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add("Pok pok");
 
         try {
-            sharedPreferences.edit().putString("arrayList",ObjectSerializer.serialize(arrayList)).apply();
+            sharedPreferences.edit().putString("arrayList", ObjectSerializer.serialize(arrayList)).apply();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> newArrayList = new ArrayList<>();
 
         try {
-            newArrayList  = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("arrayList",
+            newArrayList = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("arrayList",
                     ObjectSerializer.serialize(new ArrayList<String>())));
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,6 +49,37 @@ public class MainActivity extends AppCompatActivity {
         // sharedPreferences.edit().putString("username","Jahid").apply();
         // String username  = sharedPreferences.getString("username","");
 
-       // System.out.println("Username: " + username);
+        // System.out.println("Username: " + username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+
+            case R.id.settings:
+                Log.i("Menu Item selected", "Settings");
+
+                return true;
+
+            case R.id.help:
+                Log.i("Menu Item selected", "Help");
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
