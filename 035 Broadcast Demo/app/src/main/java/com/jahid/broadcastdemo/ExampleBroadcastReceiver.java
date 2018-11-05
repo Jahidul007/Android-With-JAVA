00599f8b2a50a7f0f77ca3f51cacabaae9373a9e
Jahidul Islam <jahidul479@gmail.com>
2018-11-05 21:25:35 +0600

    init commit

---
+++ 035 Broadcast Demo/app/src/main/java/com/jahid/broadcastdemo/ExampleBroadcastReceiver.java
@@ -0,0 +1,35 @@
+package com.jahid.broadcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+
+            boolean noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            );
+
+            if (noConnectivity) {
+
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_LONG).show();
+            }
+        }
+
+//        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+//
+//            Toast.makeText(context,"Connectivity Changed",Toast.LENGTH_LONG).show();
+//
+//        }
+
+    }
+}
