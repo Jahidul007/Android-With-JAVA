84c7e699ebdc390e242a77b468bb9adf30b90a97
Jahidul Islam <jahidul479@gmail.com>
2018-12-03 03:24:03 +0600

    Update MapsActivity.java

--- 023 Google Map demo/app/src/main/java/example/com/googlemapsdemo/MapsActivity.java
+++ 023 Google Map demo/app/src/main/java/example/com/googlemapsdemo/MapsActivity.java
@@ -1,7 +1,19 @@
 package example.com.googlemapsdemo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,19 +26,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener  {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private static  final LatLng  PERTH = new LatLng(-31.9528,115.857342);
-    private static  final LatLng  SYDNEY = new LatLng(-34.87365,151.20689);
-    private static  final LatLng  BRISBANE = new LatLng(-27.9528,153.857342);
 
-    private Marker mPerth;
-    private Marker mSydney;
-    private Marker mBribane;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+   // @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,80 +48,118 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+       /*
+       // find current location
+       if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        List<Marker>  markerList = new ArrayList<>();
+            if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M){
 
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION
+                , Manifest.permission.ACCESS_COARSE_LOCATION},1);
 
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-       // mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+            return;
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }*/
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
 
-        // Add a marker in Sydney and move the camera
-        mPerth = mMap.addMarker(new MarkerOptions()
-                .position(PERTH)
-                .title("Perth"));
-        mPerth.setTag(0);
-        markerList.add(mPerth);
+                Log.d("onLocationChanged: ", location.toString());
+                mMap.clear(); // clears our map
 
-        mSydney = mMap.addMarker(new MarkerOptions()
-                .position(SYDNEY)
-                .title("Sydney")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
-        mSydney.setTag(0);
-        markerList.add(mSydney);
+                // Add a marker in Sydney and move the camera
 
-        mBribane = mMap.addMarker(new MarkerOptions()
-                .position(BRISBANE)
-                .title("Brisbane")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
-        mBribane.setTag(0);
-        markerList.add(mBribane);
+                LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                //LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(newLocation).title("New Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
 
-        mMap.setOnMarkerClickListener(this); // register outer listener
-        for(Marker marker : markerList){
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
 
-            LatLng latLng = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
-            mMap.addMarker(new MarkerOptions().position(latLng));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,2));
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(),
+                            location.getLongitude(), 1);
 
-            //Log.d("Marker", marker.getTitle());
+                    String fullAdress = "";
+                    if (addressList != null && addressList.size() > 0) {
 
+                        Log.d("Address : ", addressList.get(0).toString());
+                        /*  Toast.makeText(MapsActivity.this, addressList.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show();
+                         */
+                        if (addressList.get(0).getAddressLine(0) != null) {
+                            fullAdress += addressList.get(0).getAddressLine(0) + " ";
+                        }
+                        if (addressList.get(0).getSubAdminArea() != null) {
+                            fullAdress += addressList.get(0).getSubAdminArea() + " ";
+                        }
+                        Toast.makeText(MapsActivity.this, "Address: " + fullAdress, Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Log.d("Address", "Location could not find");
+                    }
+
+                } catch (IOException e) {
+
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            }
         }
-       /* LatLng Sydney1 = new LatLng(-34,151);
-        LatLng Mount = new LatLng(27.988119,86.9074655);
 
-        mMap.addMarker(new MarkerOptions().position(Sydney1).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Sydney1,13));*/
+
     }
 
     @Override
-    public boolean onMarkerClick(Marker marker) {
-        Integer clickCount = (Integer) marker.getTag();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(clickCount != null){
-            clickCount++;
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-            marker.setTag(clickCount);
-            Toast.makeText(this, marker.getTitle()+" has been clicked "
-                   + clickCount +" times ", Toast.LENGTH_SHORT).show();
-            System.out.println("Count: "+ clickCount);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            }
         }
-        return false;
     }
 }
